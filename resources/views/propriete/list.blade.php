e1c625f5db20505f006545ad7b36a09e32776b4c
abdoulkarim <abdoulkarimfoly1996@gmail.com>
2022-04-01 10:24:32 +0000

    gestion des listes des proprietes

--- resources/views/propriete/list.blade.php
+++ resources/views/propriete/list.blade.php
@@ -10,12 +10,13 @@
         <th scope="col">#</th>
         <th scope="col">Libelle</th>
         <th scope="col">Superficie</th>
-        <th scope="col">Nombre de pieces</th>
+        <th scope="col">Pieces</th>
+        <th scope="col">Propriétaire</th>
         <th scope="col">Quartier</th>
         <th scope="col">Deduction</th>
         <th scope="col">Type</th>
-        <th scope="col">Propriétaire</th>
-        <th scope="col">Actions</th>
+        <th scope="col">Agence</th>
+        <th scope="col" colspan="2">Actions</th>
       </tr>
     </thead>
     <tbody>
@@ -26,9 +27,16 @@
         <td>{{$propriete->libelle}}</td>
         <td>{{$propriete->superficie}}</td>
         <td>{{$propriete->nombrePiece}}</td>
+        <td>{{$propriete->proprietaire->prenom.' '.$propriete->proprietaire->prenom}}</td>
+        <td>{{$propriete->quartier->libelle}}</td>
+        <td>{{$propriete->deduction->montant}}</td>
+        <td>{{$propriete->type->libelle}}</td>
+        <td>{{$propriete->agence->libelle}}</td>
         <td>
-            <a href="" class="btn btn-success" style="border-radius:15px; ">Modifier</a>
-            <a href="" class="btn btn-danger" style="border-radius:15px; ">Supprimer</a>
+            <a href="" class="btn btn-success" style="border-radius:15px; ">edit</a>
+        </td>
+        <td>
+        <a href="" class="btn btn-danger" style="border-radius:15px; ">del</a>
         </td>
       </tr>
 
